ab1a0575c7e92f94e15cbdf5c35aa41f5d5f70ae
Ashok Gelal <ashokgelal@gmail.com>
2014-07-17 00:45:18 -0600

    Add IDocumentsManager interface

--- Contracts/DocumentInterfaces.cs
+++ Contracts/DocumentInterfaces.cs
@@ -1,6 +1,7 @@
-﻿#region usings
+﻿#region Using
 
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Threading.Tasks;
 
@@ -31,5 +32,16 @@ namespace LightPaper.Infrastructure.Contracts
     {
         Task<bool?> DocumentShouldCloseAsync(IDocument document);
         Task<bool?> DocumentsShouldCloseAsync(IEnumerable<IDocument> documents);
+    }
+
+    public interface IDocumentsManager
+    {
+        ObservableCollection<IDocument> WorkingDocuments { get; }
+        IDocument CurrentDocument { get; }
+        bool Add(IDocument document, bool doSelect = false);
+        Task CloseCurrentAsync();
+        Task CloseAsync(IDocument document);
+        void SaveAllWorkingDocuments();
+        void PruneTransientDocuments();
     }
 }
