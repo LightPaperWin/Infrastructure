0b239988adce228d9932d0122481e135fd55902e
Ashok Gelal <ashokgelal@gmail.com>
2014-07-19 01:30:39 -0600

    Move documents related events.

---
+++ Events/Events.LightPaper.Documents.cs
@@ -0,0 +1,36 @@
+﻿#region Using
+
+using LightPaper.Infrastructure.Contracts;
+using Microsoft.Practices.Prism.PubSubEvents;
+
+#endregion
+
+namespace LightPaper.Infrastructure.Events
+{
+    public class SelectedDocumentChangedEvent : PubSubEvent<SelectedDocumentChangedEvent.Payload>
+    {
+        #region Nested type: Payload
+
+        public class Payload
+        {
+            public Payload(IDocument oldSelection, IDocument newSelection)
+            {
+                OldSelection = oldSelection;
+                NewSelection = newSelection;
+            }
+
+            public IDocument OldSelection { get; private set; }
+            public IDocument NewSelection { get; private set; }
+        }
+
+        #endregion
+    }
+
+    public class DocumentWillCloseEvent : PubSubEvent<IDocument>
+    {
+    }
+
+    public class DocumentClosedEvent : PubSubEvent<IDocument>
+    {
+    }
+}
