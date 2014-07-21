4d3dee68f1f8d76190b63d7403d5ecfd8dcda012
Ashok Gelal <ashokgelal@gmail.com>
2014-07-20 18:14:12 -0600

    Add a documents filter extension.

---
+++ Helpers/DocumentExtensions.cs
@@ -0,0 +1,18 @@
+﻿#region Using
+
+using System.Collections.Generic;
+using System.Linq;
+using LightPaper.Infrastructure.Contracts;
+
+#endregion
+
+namespace LightPaper.Infrastructure.Helpers
+{
+    public static class DocumentExtensions
+    {
+        public static IEnumerable<string> FilterValidPaths(this IEnumerable<IDocument> documents)
+        {
+            return documents.Select(d => d.SourcePath).Where(p => !string.IsNullOrWhiteSpace(p));
+        }
+    }
+}
