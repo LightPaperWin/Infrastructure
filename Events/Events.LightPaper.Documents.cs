7497c74f8bc880f07f0cd3a50fd72179ae497eb9
Ashok Gelal <ashokgelal@gmail.com>
2014-07-24 22:40:20 -0600

    Add more Documents related events.

--- Events/Events.LightPaper.Documents.cs
+++ Events/Events.LightPaper.Documents.cs
@@ -1,5 +1,6 @@
 ﻿#region Using
 
+using System.ComponentModel;
 using LightPaper.Infrastructure.Contracts;
 using Microsoft.Practices.Prism.PubSubEvents;
 
@@ -26,11 +27,34 @@ namespace LightPaper.Infrastructure.Events
         #endregion
     }
 
+    public class SelectedDocumentWillChangeEvent : PubSubEvent<SelectedDocumentWillChangeEvent.Payload>
+    {
+        #region Nested type: Payload
+
+        public class Payload
+        {
+            public Payload(IDocument currentDocument, CurrentChangingEventArgs eventArgs)
+            {
+                CurrentDocument = currentDocument;
+                ChangingEventArgs = eventArgs;
+            }
+
+            public IDocument CurrentDocument { get; private set; }
+            public CurrentChangingEventArgs ChangingEventArgs { get; private set; }
+        }
+
+        #endregion
+    }
+
     public class DocumentWillCloseEvent : PubSubEvent<IDocument>
     {
     }
 
     public class DocumentClosedEvent : PubSubEvent<IDocument>
+    {
+    }
+
+    public class DocumentAddedToWorkingCollectionEvent : PubSubEvent<IDocument>
     {
     }
 }
