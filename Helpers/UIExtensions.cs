d188792ed0783697072231f6ee8de47587571799
Ashok Gelal <ashokgelal@gmail.com>
2014-07-25 23:59:40 -0600

    Add UIExtensions

---
+++ Helpers/UIExtensions.cs
@@ -0,0 +1,30 @@
+﻿#region Using
+
+using System.Windows;
+using System.Windows.Media;
+
+#endregion
+
+namespace LightPaper.Infrastructure.Helpers
+{
+    public static class UIExtensions
+    {
+        public static T FindVisualChild<T>(this DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj == null) return null;
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+                var validChild = child as T;
+                if (validChild != null)
+                {
+                    return validChild;
+                }
+
+                var childItem = FindVisualChild<T>(child);
+                if (childItem != null) return childItem;
+            }
+            return null;
+        }
+    }
+}
